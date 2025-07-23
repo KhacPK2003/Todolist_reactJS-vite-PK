ec82b0a4decd31242f11171c7423c24413e10763
PhamKhac <phamkhac2003@gmail.com>
2025-07-23 18:14:27 +0700

    90: Chia layout Reponsive với Grid trên antd, Chia thành 24 cột chứ ko 12 cột giống bootstrap

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,88 +1,110 @@
 import React from "react";
-import { Input, Button, Form, notification, Descriptions } from "antd";
+import { Input, Button, Form, notification, Row, Col } from "antd";
 import { registerUserApi } from "../services/api.services";
 import { useNavigate } from "react-router";
 const RegisterPage = () => {
-    const [form] = Form.useForm();
-    const navigate = useNavigate();
-    const onFinish = async (values) =>{
-        console.log("<<< Check values: ",values)
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    console.log("<<< Check values: ", values);
 
-        const res = await registerUserApi(
-            values.fullName, 
-            values.email, 
-            values.password, 
-            values.phone);
+    const res = await registerUserApi(
+      values.fullName,
+      values.email,
+      values.password,
+      values.phone
+    );
 
-        if(res.data) {
-            notification.success({
-                message: "Register use",
-                Descriptions: "Đăng ký user thành công"
-            });
-            navigate("/login");
-        } else {
-            notification.error ({
-                message: "Register user error",
-                Descriptions: "Đăng ký user thất bại"
-            })
-        }
+    if (res.data) {
+      notification.success({
+        message: "Register use",
+        Descriptions: "Đăng ký user thành công",
+      });
+      navigate("/login");
+    } else {
+      notification.error({
+        message: "Register user error",
+        Descriptions: "Đăng ký user thất bại",
+      });
     }
-    
+  };
+
   return (
     <Form
-    form={form}
-    layout="vertical"
-    onFinish={onFinish}
-    // onFinishFailed={onFinishFailed}
-  >
-    <div style={{
-        margin:"50px",
-    }}>
-    <Form.Item
-      label="Full Name"
-      name="fullName"
-      rules={[{ required: true, message: 'Please input your username!' }]}
+      form={form}
+      layout="vertical"
+      onFinish={onFinish}
+      // onFinishFailed={onFinishFailed}
     >
-      <Input />
-    </Form.Item>
-    <Form.Item
-      label="Email"
-      name="email"
-      rules={[{ required: true, message: 'Please input your email!' }]}
-    >
-      <Input />
-    </Form.Item>
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}
-    >
-      <Input.Password />
-    </Form.Item>
-    <Form.Item
-      label="PhoneNumber"
-      name="phone"
-      rules={[{
-        required: true,
-        pattern: new RegExp(/\d+/g),
-        message: "Wrong format!"
-      }]}
-    >
-      <Input />
-    </Form.Item>
+      <Row>
+        <Col xs={24} md={6}>
+          <Form.Item
+            label="Full Name"
+            name="fullName"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+        </Row>
+        <Row>
+        <Col xs={24} md={6}>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+        </Row>
+        <Row>
+        <Col xs={24} md={6}>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+        </Col>
+        </Row>
+        <Row>
+        <Col xs={24} md={6}>
+          <Form.Item
+            label="PhoneNumber"
+            name="phone"
+            rules={[
+              {
+                required: true,
+                pattern: new RegExp(/\d+/g),
+                message: "Wrong format!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+        </Row>
+        <Row>
+        <div>
+          <Button onClick={() => form.submit()} type="primary">
+            Register
+          </Button>
 
-      <div>
-        <Button onClick ={() => form.submit()} type="primary">Register</Button>
-        
-        <Button onClick={()=> {
-            form.setFieldsValue({
+          <Button
+            onClick={() => {
+              form.setFieldsValue({
                 email: "khac1@gmail.com",
-                fullname:"PHamKhac"
-            })
-           console.log("<<<check form:") 
-        }}>Test</Button>
-      </div>
-    </div>
+                fullname: "PHamKhac",
+              });
+              console.log("<<<check form:");
+            }}
+          >
+            Test
+          </Button>
+        </div>
+      </Row>
     </Form>
   );
 };
